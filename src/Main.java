d6f414c08c2982f7419daf6dcc1187b48d794159
gibclay <momch4il@gmail.com>
2024-02-17 19:22:08 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * This is the main file of the project, it runs the project.
+ *
+ */
+public class Main {
+  public static void main(String[] args) { System.out.println("hello world"); }
+}
+
