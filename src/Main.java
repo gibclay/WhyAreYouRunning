79ebcc7cbe331a6c229e81f88d48456c19ea124a
0-20 <zerodashtwenty@protonmail.com>
2024-03-10 21:44:47 -0400

    Initial Python Commit

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * This is the main file of the project, it runs the project.
- *
- */
-public class Main {
-  public static void main(String[] args) { System.out.println("hello world"); }
-}
-
